e7662a44e306f0483188607cc85697e8bc960da0
MartinXCVI <mdzg96@gmail.com>
2025-04-24 23:27:16 -0300

    Initial commit

---
+++ src/components/ReactComponent.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react"
+
+const ReactComponent = (): JSX.Element => {
+  return (
+    <h1 className="text-purple-700">Hello from React Component with Tailwind</h1>
+  )
+}
+
+export default ReactComponent
