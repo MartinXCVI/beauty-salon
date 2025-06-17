e86e3e32f1d03607f043e89ab1986ab4baa4d60e
MartinXCVI <mdzg96@gmail.com>
2025-06-16 22:20:49 -0300

    Improvements to home page related to content, style and accessibility.

--- src/components/Home/BannerSlider.tsx
+++ src/components/Home/BannerSlider.tsx
@@ -9,9 +9,9 @@ import type { FunctionComponent } from 'react';
 
 const BannerSlider: FunctionComponent<ImageCarouselProps> = ({ images }) => {
   return (
-    <div className="relative w-full min-h-[90vh] h-[90vh] overflow-hidden mx-auto">
+    <div className="relative w-full min-h-screen overflow-hidden mx-auto">
       <Swiper
-      className='home-banner-swiper min-h-[90vh] h-[90vh]'
+        className='home-banner-swiper min-h-screen'
         modules={[Navigation, Autoplay, A11y]}
         spaceBetween={20}
         slidesPerView={1}
@@ -28,24 +28,33 @@ const BannerSlider: FunctionComponent<ImageCarouselProps> = ({ images }) => {
           prevSlideMessage: 'Previous slide',
           nextSlideMessage: 'Next slide',
         }}
+        aria-live='polite'
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
-            <img
-              src={image.src}
-              alt={image.alt}
-              className="w-full h-full object-cover opacity-90 cursor-grab"
-            />
+            <div className="relative w-full h-screen">
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="w-full h-full object-cover brightness-[.85] transition-transform
+                duration-700 ease-in-out scale-100 hover:scale-105 cursor-grab"
+              />
+              <div className="absolute inset-0 bg-black/20" aria-hidden="true" />
+            </div>
           </SwiperSlide>
         ))}
 
         {/* Navigation Buttons */}
         <button
-          className="swiper-button-prev text-black absolute left-4 top-1/2 z-10 -translate-y-1/2"
+          className="swiper-button-prev absolute left-4 top-1/2 z-20 -translate-y-1/2 
+          bg-white/20 hover:bg-white/40 text-white p-5 backdrop-blur-md 
+          shadow-md transition-all duration-300"
           aria-label="Previous Slide"
         ></button>
         <button
-          className="swiper-button-next text-black absolute right-4 top-1/2 z-10 -translate-y-1/2"
+          className="swiper-button-next absolute right-4 top-1/2 z-20 -translate-y-1/2 
+          bg-white/20 hover:bg-white/40 text-white p-5 backdrop-blur-md 
+          shadow-md transition-all duration-300"
           aria-label="Next Slide"
         ></button>
       </Swiper>
